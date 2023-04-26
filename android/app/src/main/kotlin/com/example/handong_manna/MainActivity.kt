4cac3c8fadda0a5465c85d2d9a8d667ec11a593a
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2023-04-26 22:44:42 +0900

    First commit

---
+++ android/app/src/main/kotlin/com/example/handong_manna/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.handong_manna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,5 @@
+// import 'package:flutter/material.dart';
+
+// class ChatPage extends StatefulWidget {
+
+// }
